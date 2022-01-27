8c0e43d7ec12352344d896c343d51952fdf95f7f
pz2105 <85478968+pz2105@users.noreply.github.com>
2022-01-27 12:03:14 -0800

    new

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,29 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+import java.io.IOException;
+
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.nio.file.Files;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+
+    @Test
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("/Users/peterzhong/Documents/GitHub/markdown-parse/test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = new ArrayList<>();
+        links.add("https://something.com");
+        links.add("some-page.html");
+        assertEquals(links, MarkdownParse.getLinks(contents));
     }
 }
